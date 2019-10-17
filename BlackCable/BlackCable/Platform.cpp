0da605eeb7f316a0e79874da4dfe38396bb4fbe6
Adidier <adidier.perez@gmail.com>
2019-10-17 08:54:51 -0500

    Added gameloop

---
+++ BlackCable/BlackCable/Platform.cpp
@@ -0,0 +1,26 @@
+#include "Platform.h"
+
+Platform::Platform(std::string name)
+{
+
+}
+
+Platform::~Platform()
+{
+
+}
+
+void Platform::RenderClear()
+{
+
+}
+
+void Platform::RenderPresent()
+{
+
+}
+
+void Platform::CheckEvent(GameState* obj, bool (GameState::* f)(int))
+{
+
+}
